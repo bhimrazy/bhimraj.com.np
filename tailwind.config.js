5ad7387ee8a032e2591c67826b84d47ea849d192
bhimrazy <bhimrajyadav977@gmail.com>
2021-04-07 20:38:42 +0545

    Initial Setup done and header part completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        mono: ["Menlo", ...defaultTheme.fontFamily.mono],
+        source: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        "ubuntu-mono": ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
+        system: defaultTheme.fontFamily.sans,
+        flow: "Flow",
+      },
+      colors: {
+        gray: colors.trueGray,
+        red: colors.red,
+        blue: colors.lightBlue,
+        yellow: colors.amber,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
