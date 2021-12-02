bea990034e6130aeb083789a94d44d37dd908b05
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-02 12:11:31 +0000

    Updated the maxwidth of webpage.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer>
+        <footer className="flex flex-col px-4 lg:px-0 w-full max-w-7xl mx-auto">
             <p>Hello From footer.</p>
         </footer>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "src/components/layout";
 export default function Home() {
     return (
         <Layout>
-            <main className="min-h-screen ">
+            <main className="flex flex-col px-4 lg:px-0 w-full max-w-7xl mx-auto min-h-screen ">
                 <h1>Hello from my homepage.</h1>
             </main>
         </Layout>
