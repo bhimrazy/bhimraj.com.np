3e7cd8ce260c13cf1b72e1351bd1f78c86f60a36
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-02 11:27:27 +0000

    Header Part Completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,16 @@ export default function Header() {
     const button_content = "Sign up ->"
     return (
         <header className="flex w-full place-content-center border-b py-4 ">
-           
+            <nav className="flex justify-between w-full max-w-7xl mx-auto items-center">
+                <ul className="flex space-x-2 font-medium text-gray-700">
+                    {nav_items.map((item, idx) => (
+                        <li className="hover:text-gray-900 cursor-pointer" key={idx}>{item}</li>
+                    ))}
+                </ul>
+                <button className="bg-blue-600 text-white px-3 py-[6px] rounded-md hover:bg-white border border-opacity-0 hover:border-opacity-100 hover:text-blue-700 border-blue-600">
+                    {button_content}
+                </button>
+            </nav>
         </header>
     )
 }
