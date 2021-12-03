2ea21d2677d173c8f4551d9012cc4f39cb7e707d
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-03 11:41:00 +0000

    Added few hero content.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import Layout from 'src/components/layout'
 
 export default function Home() {
+  const hero_content: { [key: string]: string } = {
+    greetings: "Hi there, I'm Bhimraj.",
+    // heading: "The Future CEO of \n Google.",
+    description: "I am a Computer Engineering Student at Kathford \
+                  International College of Engineering and Management. \
+                  I like making websites with NextJs and Tailwind\s CSS and \
+                  playing with AI tools.\
+                  "
+  }
   return (
     <Layout>
       <main className="flex flex-col px-4 xl:px-0 w-full max-w-5xl mx-auto">
         <section className="flex flex-col justify-center text-center space-y-3 items-center h-[85vh]">
-          <span className='text-lg md:text-xl font-semibold'>Hi there,</span>
-          <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold">The Future of Writing <br /> SEO Content.</h1>
+          <span className='text-lg md:text-xl font-semibold'>{hero_content?.greetings}</span>
+          <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold">{hero_content?.heading}</h1>
           <p className=" max-w-2xl md:text-lg mx-auto">
-            Artificial intelligence makes it fast & easy to create content for your blog, social
-            media, website, and more! Rated 5/5 stars in 1,000+ reviews.
+            {hero_content?.description}
           </p>
           <div className='justify-center py-4'>
             <button className="bg-blue-600 text-white text-lg px-8 py-2 rounded-md hover:bg-white border border-opacity-0 hover:border-opacity-100 hover:text-blue-700 border-blue-600">
