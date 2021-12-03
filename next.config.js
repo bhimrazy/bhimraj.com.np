748d2abb95974125859c7f2f2715621a3ae641b9
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-03 13:51:32 +0000

    Updated config file for device sizes,formats and domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     formats: ['image/avif', 'image/webp'],
     domains:['i3.ytimg.com']
   },
