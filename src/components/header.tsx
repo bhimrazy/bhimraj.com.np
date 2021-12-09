ff23063890b7e218b97cdcd9b73c0055e84bf662
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-09 10:54:18 +0000

    Updated header for dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,24 +11,24 @@ export default function Header() {
   const button_content = 'Sign up ->'
   return (
     <>
-      <p className="text-sm tracking-widest uppercase font-medium text-center py-2 bg-gray-50  cursor-pointer">
+      <p className="text-sm tracking-widest uppercase font-medium text-center py-2 bg-gray-50 dark:bg-black  cursor-pointer">
         {top_bar}
       </p>
-      <header className="flex flex-col bg-white sticky top-0 z-10 w-full place-content-center border-t border-b">
+      <header className="flex flex-col bg-white dark:bg-black sticky top-0 z-10 w-full place-content-center border-t border-b dark:border-gray-900">
         <nav className="flex justify-between px-4 py-4 xl:px-0 w-full max-w-5xl mx-auto items-center">
           <Link href="/">
-            <a className="text-base font-medium text-gray-900 uppercase tracking-wider">
+            <a className="text-base font-medium text-gray-900 dark:text-gray-100 uppercase tracking-wider">
               {icon_name}
             </a>
           </Link>
-          <ul className="hidden sm:flex space-x-2 sm:space-x-4 md:space-x-5 lg:space-x-6 font-medium text-gray-600">
+          <ul className="hidden sm:flex space-x-2 sm:space-x-4 md:space-x-5 lg:space-x-6 font-medium text-gray-600 dark:text-gray-100">
             {nav_items.map((item, idx) => (
               <Link href={item?.link} key={idx}>
                 <a>
                   <li
                     className={`${
-                      item?.is_home ? 'text-gray-900' : ''
-                    } hover:text-blue-500 cursor-pointer`}
+                      item?.is_home ? 'text-gray-900 dark:text-gray-100' : ''
+                    } hover:text-blue-500 dark:hover:text-blue-400 cursor-pointer`}
                   >
                     {item?.title}
                   </li>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ import Footer from './footer'
 
 export default function Layout({children,meta_data}:{children:ReactNode,meta_data:any}) {
     return (
-        <div className="flex font-display flex-col antialiased bg-white">
+        <div className="flex font-display flex-col antialiased bg-white dark:bg-black dark:text-white">
             <Head>
                 <title>{meta_data?.title}</title>
             </Head>
