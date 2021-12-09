326a4a99f8e7552918230c35e4f73dc6b6413a8a
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-09 10:59:23 +0000

    Updated newsletter section for dark mode

--- src/components/homepage/news_letter.tsx
+++ src/components/homepage/news_letter.tsx
@@ -8,12 +8,12 @@ export default function NewsLetter() {
   }
   return (
     <section className="pt-10 pb-16">
-      <div className="flex flex-col border rounded-lg py-14 text-center mx-auto space-y-6">
+      <div className="flex flex-col border dark:border-gray-900 rounded-lg py-14 text-center mx-auto space-y-6">
         <div className="space-y-1">
           <h2 className="text-2xl font-semibold">{newsletter_content?.title}</h2>
           <p className="text-gray-600">{newsletter_content?.description}</p>
         </div>
-        <div className="relative sm:w-96 max-w-2xl mx-auto border rounded">
+        <div className="relative sm:w-96 max-w-2xl mx-auto border dark:border-gray-900 rounded">
           <input
             className="px-6 py-2 w-full text-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-600 ring-offset-2 rounded"
             type="email"
