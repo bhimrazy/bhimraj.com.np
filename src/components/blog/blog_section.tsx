15f12ead48260808cbcc801c9a48a7611ae285d0
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-09 11:31:12 +0000

    Added few roperties for dark mode

--- src/components/blog/blog_section.tsx
+++ src/components/blog/blog_section.tsx
@@ -6,10 +6,10 @@ export default function BlogSection({blogs}) {
   return (
     <section className="flex flex-col py-10 md:py-16">
       <h1 className="text-5xl  font-bold md:font-black">{blog_content?.title}</h1>
-      <div className="flex flex-col prose lg:prose-lg ">
+      <div className="flex flex-col prose lg:prose-lg dark:text-gray-200">
         {blog_content?.blogs.map((blog,idx)=>(
-          <article key={idx} className="">
-            <a href="#"><h2>{blog?.data?.title}</h2></a>
+          <article key={idx}>
+            <a href="#"><h2 className="dark:text-gray-200">{blog?.data?.title}</h2></a>
             <p>{blog?.data?.summary}</p>
           </article>
         ))}
