b07188f8aed9a072b930a3089e6323d4660e071c
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-09 11:12:21 +0000

    Updated news letter input section for dark mode

--- src/components/homepage/news_letter.tsx
+++ src/components/homepage/news_letter.tsx
@@ -13,14 +13,14 @@ export default function NewsLetter() {
           <h2 className="text-2xl font-semibold">{newsletter_content?.title}</h2>
           <p className="text-gray-600">{newsletter_content?.description}</p>
         </div>
-        <div className="relative sm:w-96 max-w-2xl mx-auto border dark:border-gray-900 rounded">
+        <div className="relative sm:w-96 max-w-2xl mx-auto border dark:border-none rounded">
           <input
-            className="px-6 py-2 w-full text-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-600 ring-offset-2 rounded"
+            className="px-6 py-2 w-full text-gray-700 dark:bg-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-600 dark:focus:ring-gray-800 focus:ring-offset-2 dark:focus:ring-offset-gray-800 rounded"
             type="email"
             placeholder={newsletter_content?.input_placeholder}
             required
           />
-          <button className="absolute inset-y-0 right-0 px-3 m-1 text-sm font-semibold text-gray-600 bg-gray-200 hover:bg-gray-200/80 rounded">
+          <button className="absolute inset-y-0 right-0 px-3 m-1 text-sm font-semibold text-gray-600 bg-gray-200 dark:bg-gray-800 dark:text-gray-400 hover:bg-gray-200/80 dark:hover:bg-gray-700 rounded">
             {newsletter_content?.button}
           </button>
         </div>
