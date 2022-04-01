69e226e1493b172053523be95528472fcef486dd
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-04-01 15:46:11 +0000

    Updated blog individual page.

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,19 +1,62 @@
-import BlogSection from "src/components/blog/blog_section";
+import { useRouter } from "next/router";
+import { get_posts } from "src/lib/utils";
 import Layout from "src/components/layout";
 import { GetStaticProps, GetStaticPaths } from "next";
-import { get_posts } from "src/lib/utils";
 
 export default function Blog({ post }) {
   const meta_data = {
     title: "BLOG | BHIMRAJ YADAV",
   };
+  const router = useRouter();
+
+  const post_content = post.content.split("\n").splice(1).join("\n");
   return (
     <Layout meta_data={meta_data}>
       <main className="mx-auto flex min-h-screen w-full max-w-5xl flex-col space-y-10 px-4 xl:px-0">
-        <section className="flex flex-col py-10 md:py-16">
-          <article className="prose dark:prose-invert lg:prose-lg">
-            <h2>{post?.data?.title}</h2>
-            <div dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+        <div className="absolute -top-28 left-3/4 hidden h-full w-28 -rotate-45 bg-gradient-to-r from-indigo-600/80 via-sky-600/75 to-purple-600/80 blur-[150px]  dark:block"></div>
+        <section className="flex flex-col py-6">
+          <article className="flex flex-col">
+            <div className="space-y-2 py-10 text-center">
+              <dl className="hidden md:block">
+                <dt className="sr-only">Date</dt>
+                <dd className="text-slate-700 dark:text-slate-400">
+                  <time dateTime="2022-02-24T12:00:00.000Z">
+                    Thursday, Febuary 24, 2022
+                  </time>
+                </dd>
+              </dl>
+              <h1 className="text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-200 sm:text-4xl">
+                {post?.data?.title}
+              </h1>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-6">
+              <div>
+                <a
+                  className="group flex font-semibold text-slate-700 hover:text-slate-900 dark:text-slate-200 dark:hover:text-white"
+                  onClick={() => router.back()}
+                >
+                  <svg
+                    viewBox="0 -9 3 24"
+                    className="mr-3 h-6 w-auto overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-300"
+                  >
+                    <path
+                      d="M3 0L0 3L3 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                  Go back to blog
+                </a>
+              </div>
+              <div
+                className="prose prose-lg prose-slate col-span-5 dark:text-slate-400"
+                dangerouslySetInnerHTML={{ __html: post_content }}
+              ></div>
+            </div>
           </article>
         </section>
       </main>
