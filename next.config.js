f17488252ad8ccdbc487f883034337191b3c7026
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-04-24 12:53:51 +0000

    Added scripts to generate rss and sitemap

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     formats: ['image/avif', 'image/webp'],
-    domains:['i3.ytimg.com','pbs.twimg.com']
+    domains: ['i3.ytimg.com', 'pbs.twimg.com']
   },
 };
---
+++ src/scripts/generate-sitemap.mjs
@@ -0,0 +1,48 @@
+import { writeFileSync } from 'fs';
+import { globby } from 'globby';
+import prettier from 'prettier';
+
+async function generate() {
+    const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+    const pages = await globby([
+        './src/pages/*.tsx',
+        './src/pages/*/*.tsx',
+        './src/content/*/*.mdx',
+        '!./src/pages/*/[slug].tsx',
+        '!./src/content/*.mdx',
+        '!./src/pages/_*.tsx',
+        '!./src/pages/api',
+        '!./src/pages/404.tsx'
+    ]);
+    const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+            .map((page) => {
+                const path = page
+                    .replace('./src/pages', '')
+                    .replace('./src/content', '')
+                    .replace('/index', '/')
+                    .replace('.tsx', '')
+                    .replace('.mdx', '');
+                const route = path === '/index' ? '' : path;
+                return `
+              <url>
+                  <loc>${`https://www.bhimraj.com.np${route}`}</loc>
+              </url>
+            `;
+            })
+            .join('')}
+    </urlset>
+    `;
+
+    const formatted = prettier.format(sitemap, {
+        ...prettierConfig,
+        parser: 'html'
+    });
+
+    // eslint-disable-next-line no-sync
+    writeFileSync('public/sitemap.xml', formatted);
+}
+
+generate();
