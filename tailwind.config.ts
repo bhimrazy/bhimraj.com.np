88174476521cd991b2f51a040c3047beeeb23230
bhimrazy <bhimrajyadav977@gmail.com>
2024-04-16 23:38:18 +0545

    Add new font size utility class and blob animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontSize: {
+        "2xs": ['10px', '12px'],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -26,12 +29,30 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "blob": {
+          "0%": {
+            transform: "translate(0px,0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px,-50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px,20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px,0px) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "blob": "blob 7s infinite"
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('@tailwindcss/typography'),
+  ],
 }
