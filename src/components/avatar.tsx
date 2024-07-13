361cd62e3925ad3a8c5cc89b375f8ae3a08ccfc3
bhimrazy <bhimrajyadav977@gmail.com>
2024-07-13 21:45:24 +0545

    Refactor Avatar component and add @radix-ui/react-avatar package

---
+++ src/components/avatar.tsx
@@ -0,0 +1,15 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { siteConfig } from "@/config/site"
+
+export function AvatarIcon() {
+    return (
+        <Avatar className="h-6 w-6">
+            <AvatarImage src={siteConfig.author.avatar} alt={siteConfig.author.name} />
+            <AvatarFallback>BR</AvatarFallback>
+        </Avatar>
+    )
+}
